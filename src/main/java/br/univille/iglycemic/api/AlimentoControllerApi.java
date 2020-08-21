425ac9443f6ececa4e64850d520139abd29c80be
CHRISTOPHER RAMOS MIRANDA <christopher.miranda@univillebr.onmicrosoft.com>
2020-08-21 01:19:15 +0000

    API Alimentos v1

---
+++ src/main/java/br/univille/iglycemic/api/AlimentoControllerApi.java
@@ -0,0 +1,32 @@
+package br.univille.iglycemic.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.univille.iglycemic.model.Alimento;
+import br.univille.iglycemic.service.AlimentoService;
+
+@RestController
+@RequestMapping("/api/v1/alimentos")
+public class AlimentoControllerApi {
+    @Autowired
+    private AlimentoService service;
+
+    @GetMapping
+    public ResponseEntity<List<Alimento>> getAll() {
+        List<Alimento> lista = service.getAll();
+        return new ResponseEntity<>(lista, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Alimento> getById(@PathVariable("id") Alimento alimento) {
+        return new ResponseEntity<>(alimento, HttpStatus.OK);
+    }
+}
--- src/main/java/br/univille/iglycemic/model/Refeicao.java
+++ src/main/java/br/univille/iglycemic/model/Refeicao.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
